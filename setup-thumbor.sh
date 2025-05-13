76d59638cc6534786a6a8f19f4d282d80e7de792
John <jmtaylor.dev@gmail.com>
2025-05-13 17:24:26 +1000

    - Sorted Docker install

--- setup-thumbor.sh
+++ setup-thumbor.sh
@@ -4,13 +4,25 @@ set -e
 
 echo "Setting up Thumbor..."
 
+echo -e "For more information, visit:\nhttps://github.com/thumbor/thumbor?tab=readme-ov-file"
+
+echo "..."
+
 # function to install Docker
 install_docker() {
-    echo "Installing Docker..."
-    sudo apt-get update
-    sudo apt-get install -y docker.io
-    sudo systemctl start docker
-    sudo systemctl enable docker
+    echo "🐳 Docker not found. Installing Docker..."
+
+    sudo apt update
+    sudo apt install -y apt-transport-https ca-certificates curl software-properties-common gnupg lsb-release
+
+    curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo gpg --dearmor -o /usr/share/keyrings/docker-archive-keyring.gpg
+
+    echo \
+      "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-archive-keyring.gpg] https://download.docker.com/linux/ubuntu \
+      $(lsb_release -cs) stable" | sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+    sudo apt update
+    sudo apt install -y docker-ce docker-ce-cli containerd.io
 }
 
 # Check if Docker is already installed
@@ -19,3 +31,12 @@ if ! command -v docker &> /dev/null; then
 else
     echo "Docker is already installed."
 fi
+
+# Check if Docker is running
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# Set up a Thumbor container
+echo "Setting up a Thumbor container..."
+
+
